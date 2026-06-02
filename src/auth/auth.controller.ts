65299c7ce12e29dce93c8ca49ac58ba6833e4fac
dinhvan <dinhvanle9xpro@gmail.com>
2026-06-02 21:15:55 +0700

    revoke update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -41,7 +41,7 @@ export class AuthController {
 
   @Post('logout')
   logout(@Req() req: any) {
-    return this.authService.logout(req.user.sub);
+    return this.authService.logout(req.user.sessionId);
   }
 
   @Public()
